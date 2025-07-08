a350f787cd650b975f60a046f409eac75ce04e26
Denii-student <24099546@student.hhs.nl>
2025-07-08 14:49:12 +0200

    Update LineFollower.cpp

--- LineFollower.cpp
+++ LineFollower.cpp
@@ -1,38 +1,60 @@
 #include "LineFollower.h"
 
+// Constructor: ontvangt gedeelde lijnsensor en stelt max snelheid in
 LineFollower::LineFollower(Zumo32U4LineSensors& sharedSensors, uint16_t maxSpeed)
   : lineSensors(sharedSensors), maxSpeed(maxSpeed)
 {
-  lineSensors.initFiveSensors();
+  lineSensors.initFiveSensors(); // Initialiseer de sensorarray
 }
 
 void LineFollower::calibrate()
 {
-  delay(1000);
-  for (uint16_t i = 0; i < 100; i++)
+  delay(1000); // Wacht even voor calibratie begint
+
+  // Calibratie: draai 2 rondjes (120 stappen) en neem sensorwaarden op
+  for (uint16_t i = 0; i < 120; i++)
   {
-    lineSensors.calibrate(); // Beweeg handmatig over zwarte lijn
-    delay(20);
+    if (i > 30 && i <= 90)
+    {
+      motors.setSpeeds(-200, 200); // Draai linksom
+    }
+    else
+    {
+      motors.setSpeeds(200, -200); // Draai rechtsom
+    }
+
+    lineSensors.calibrate(); // Registreer sensorwaarden
   }
+
+  motors.setSpeeds(0, 0); // Stop na calibratie
 }
 
 void LineFollower::followLine()
 {
+  // Lees lijnpositie: 0 = helemaal links, 4000 = helemaal rechts, 2000 = midden
   int16_t position = lineSensors.readLine(sensorValues);
+
+  // Bereken afwijking van het midden
   int16_t error = position - 2000;
-  int16_t speedDifference = error / 6 + 4 * (error - lastError);
+
+  // Bereken snelheidsverschil met PID (alleen P & D)
+  int16_t speedDifference = error / 4 + 6 * (error - lastError);
   lastError = error;
 
+  // Stel linker- en rechtersnelheid in
   int16_t leftSpeed = (int16_t)maxSpeed + speedDifference;
   int16_t rightSpeed = (int16_t)maxSpeed - speedDifference;
 
+  // Beperk snelheden tot geldige range
   leftSpeed = constrain(leftSpeed, 0, (int16_t)maxSpeed);
   rightSpeed = constrain(rightSpeed, 0, (int16_t)maxSpeed);
 
+  // Pas motoren aan
   motors.setSpeeds(leftSpeed, rightSpeed);
 }
 
 void LineFollower::stop()
 {
+  // Zet beide motoren stil
   motors.setSpeeds(0, 0);
 }

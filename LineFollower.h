ff283ff573f7399ab5a5e301a1d2cb16ae3f033e
Denii-student <24099546@student.hhs.nl>
2025-07-08 14:38:48 +0200

    Update LineFollower.h
    back

--- LineFollower.h
+++ LineFollower.h
@@ -1,25 +1,23 @@
-#ifndef LINE_FOLLOWER_H
-#define LINE_FOLLOWER_H
+#ifndef COLOR_SENSOR_H
+#define COLOR_SENSOR_H
 
 #include <Zumo32U4.h>
 
-class LineFollower
+class ColorSensor
 {
 public:
-    LineFollower(Zumo32U4LineSensors& sharedSensors, uint16_t maxSpeed = 400);
-    void calibrate();    // Kalibreert op zwarte lijn
-    void followLine();   // Volgt de lijn met PID
-    void stop();         // Stopt beide motoren
+    ColorSensor(Zumo32U4LineSensors& sharedSensors);
+    bool detectBrown();  // Retourneert true als bruine lijn is gezien
 
 private:
     Zumo32U4LineSensors& lineSensors;
-    Zumo32U4Motors motors;
 
     static const uint8_t numSensors = 5;
     unsigned int sensorValues[numSensors];
 
-    int16_t lastError = 0;
-    uint16_t maxSpeed;
+    // Afkomstig uit jouw testen
+    uint16_t brownMin = 300;
+    uint16_t brownMax = 700;
 };
 
 #endif

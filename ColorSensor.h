7b956e53ba96271309e29eaedd5f88984ad55e15
Denii-student <24099546@student.hhs.nl>
2025-07-08 14:20:00 +0200

    Update ColorSensor.h
    reset

--- ColorSensor.h
+++ ColorSensor.h
@@ -3,34 +3,20 @@
 
 #include <Zumo32U4.h>
 
-/*
- * Klasse: ColorSensor
- * Doel: Detecteert een bruine lijn onder de robot
- * Werkt op basis van reflectiewaarden van de 5 lijnsensoren van de Zumo
- * Detectie is gebaseerd op gemiddelde reflectie én het aantal actieve sensoren
- */
 class ColorSensor
 {
 public:
     ColorSensor(Zumo32U4LineSensors& sharedSensors);
-    // Constructor: gebruikt gedeelde lijnsensor die al gekalibreerd is via LineFollower
-
-    bool detectBrown();
-    // Retourneert true als een horizontale bruine lijn onder de robot wordt herkend
+    bool detectBrown();  // Retourneert true als bruine lijn is gedetecteerd
 
 private:
     Zumo32U4LineSensors& lineSensors;
-    // Referentie naar gedeelde lijnsensor-array
 
     static const uint8_t numSensors = 5;
-    // Aantal lijnsensoren op de Zumo
-
     unsigned int sensorValues[numSensors];
-    // Sensorwaarden van de huidige meting
 
-    uint16_t brownMin = 250;
-    uint16_t brownMax = 330;
-    // Op basis van jouw metingen: gemiddelde waarde voor bruin ligt tussen 250 en 330
+    uint16_t brownMin = 300;
+    uint16_t brownMax = 700;
 };
 
 #endif

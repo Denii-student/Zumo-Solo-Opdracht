0e4336ad200bb91a718d539eca4fce096bc03e22
Denii-student <24099546@student.hhs.nl>
2025-07-08 14:49:42 +0200

    Update LineFollower.h

--- LineFollower.h
+++ LineFollower.h
@@ -1,23 +1,46 @@
-#ifndef COLOR_SENSOR_H
-#define COLOR_SENSOR_H
+#ifndef LINE_FOLLOWER_H
+#define LINE_FOLLOWER_H
 
 #include <Zumo32U4.h>
 
-class ColorSensor
+/*
+ * Klasse: LineFollower
+ * Doel: Laat de robot een lijn volgen met behulp van PID-regeling
+ * Gebruikt een gedeelde lijnsensor en stuurt de motoren aan op basis van lijnpositie
+ */
+class LineFollower
 {
 public:
-    ColorSensor(Zumo32U4LineSensors& sharedSensors);
-    bool detectBrown();  // Retourneert true als bruine lijn is gezien
+    LineFollower(Zumo32U4LineSensors& sharedSensors, uint16_t maxSpeed = 400);
+    // Constructor: gebruikt gedeelde sensor en stelt max snelheid in (standaard 400)
+
+    void calibrate();
+    // Draait op eigen plek en kalibreert de lijnsensor
+
+    void followLine();
+    // Laat de robot een lijn volgen met een PID-regeling
+
+    void stop();
+    // Zet beide motoren stil
 
 private:
     Zumo32U4LineSensors& lineSensors;
+    // Referentie naar gedeelde lijnsensor (komt uit main)
+
+    Zumo32U4Motors motors;
+    // Zumo-motorcontroller
 
     static const uint8_t numSensors = 5;
-    unsigned int sensorValues[numSensors];
+    // Aantal sensoren in de lijnsensor-array
 
-    // Afkomstig uit jouw testen
-    uint16_t brownMin = 300;
-    uint16_t brownMax = 700;
+    unsigned int sensorValues[numSensors];
+    // Array met huidige sensorwaarden (voor PID en debug)
+
+    int16_t lastError = 0;
+    // Laatste foutwaarde van lijnpositie (voor D-term)
+
+    uint16_t maxSpeed;
+    // Maximale motorsnelheid (standaard 400)
 };
 
 #endif

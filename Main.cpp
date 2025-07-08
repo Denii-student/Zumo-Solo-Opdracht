1f76339a1d064ef118808a385c098a7906ec65d6
Denii-student <24099546@student.hhs.nl>
2025-07-08 11:30:49 +0200

    Update Main.cpp
    STOP at color brown test

--- Main.cpp
+++ Main.cpp
@@ -1,14 +1,28 @@
 #include "LineFollower.h"
+#include "ColorSensor.h"
 
+// Maak objecten aan voor lijnvolging en kleurdetectie
 LineFollower lijnvolger;
+ColorSensor kleurSensor;
 
 void setup()
 {
+  // Start de calibratie op de groene lijn (of zwart als je dat gebruikt)
   lijnvolger.calibrate();
 }
 
 void loop()
 {
-  lijnvolger.followLine();
-}
+  // Als een bruine lijn wordt gedetecteerd:
+  if (kleurSensor.detectBrown()) {
+    // Stop de robot
+    lijnvolger.motors.setSpeeds(0, 0);
 
+    // Pauze zodat je ziet dat hij gestopt is (10 seconden)
+    delay(10000);
+  }
+  else {
+    // Anders gewoon de lijn volgen
+    lijnvolger.followLine();
+  }
+}

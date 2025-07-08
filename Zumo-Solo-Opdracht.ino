a7ee4be1e1683bf8d007c6e0b91e123b06eef58c
Denii-student <24099546@student.hhs.nl>
2025-07-08 14:39:39 +0200

    Update Zumo-Solo-Opdracht.ino
    back

--- Zumo-Solo-Opdracht.ino
+++ Zumo-Solo-Opdracht.ino
@@ -3,40 +3,31 @@
 #include "ColorSensor.h"
 
 Zumo32U4LineSensors gedeeldeSensor;
-Zumo32U4ButtonA buttonA;
-
 LineFollower lijnvolger(gedeeldeSensor);
 ColorSensor kleurSensor(gedeeldeSensor);
-
-bool inSumoMode = false;
 
 void setup()
 {
   Serial.begin(9600);
-  Serial.println("Plaats robot op zwarte lijn en druk op knop A");
+  Serial.println("Kalibratie op zwart gestart...");
 
-  while (!buttonA.getSingleDebouncedPress());
+  delay(1000);
+  for (uint8_t i = 0; i < 100; i++)
+  {
+    gedeeldeSensor.calibrate();
+    delay(20);
+  }
 
-  lijnvolger.calibrate();
-  Serial.println("Kalibratie voltooid ✅");
+  Serial.println("Kalibratie voltooid. Start met lijn volgen.");
 }
 
 void loop()
 {
-  if (inSumoMode)
-  {
-    // ➕ Hier komt later jouw sumo-mode gedrag
-    Serial.println("➤ SUMO MODE ACTIEF");
-    // bijv: draai en duw
-    return;
-  }
-
   if (kleurSensor.detectBrown())
   {
     lijnvolger.stop();
-    Serial.println("Bruine lijn gedetecteerd, overschakelen naar sumo...");
-    delay(1000);
-    inSumoMode = true;
+    Serial.println("Bruine lijn gedetecteerd, robot stopt.");
+    delay(10000); // zichtbare stop
   }
   else
   {

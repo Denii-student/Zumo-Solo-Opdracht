170485ce72a21576e01c11787dc15bc0344714b3
Denii-student <24099546@student.hhs.nl>
2025-07-08 15:09:46 +0200

    Update Zumo-Solo-Opdracht.ino

--- Zumo-Solo-Opdracht.ino
+++ Zumo-Solo-Opdracht.ino
@@ -1,5 +1,6 @@
 #include "LineFollower.h"
 #include "ColorSensor.h"
+#include "SumoBehavior.h"
 #include <Zumo32U4.h>
 
 // ✅ Gedeelde lijnsensor wordt aangemaakt in main
@@ -8,23 +9,30 @@ Zumo32U4LineSensors gedeeldeSensor;
 // ✅ Klassen krijgen allebei toegang tot dezelfde sensor
 LineFollower lijnvolger(gedeeldeSensor);
 ColorSensor kleurSensor(gedeeldeSensor);
+SumoBehavior sumoGedrag;
+
+bool inSumoMode = false;
 
 void setup()
 {
-  Serial.begin(9600); // Start seriële communicatie voor debug
-  lijnvolger.calibrate(); // Kalibreer op groene lijn (herkent dan ook zwart)
+  Serial.begin(9600);                 // Debug via Serial Monitor
+  lijnvolger.calibrate();            // Kalibreer op zwarte lijn
 }
 
 void loop()
 {
-  // Check of robot op een bruine lijn staat
-  if (kleurSensor.detectBrown())
+  if (inSumoMode)
   {
-    lijnvolger.stop(); // Stop robot
-    delay(10000);      // Pauze van 10 seconden om dit zichtbaar te maken
+    sumoGedrag.engage();             // Sumo modus: blok detecteren en duwen
+  }
+  else if (kleurSensor.detectBrown())
+  {
+    inSumoMode = true;               // Activeer sumo-modus
+    lijnvolger.stop();               // Voor de zekerheid stoppen voor overgang
+    delay(500);                      // Korte pauze voor overgang
   }
   else
   {
-    lijnvolger.followLine(); // Normaal lijngedrag met PID
+    lijnvolger.followLine();         // Volg lijn zolang bruin niet is gedetecteerd
   }
 }

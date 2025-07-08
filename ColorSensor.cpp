f1eac0aaf0e49be018792243675241d37046a1c1
Denii-student <24099546@student.hhs.nl>
2025-07-08 12:11:31 +0200

    Update ColorSensor.cpp
    fix brown line detection

--- ColorSensor.cpp
+++ ColorSensor.cpp
@@ -7,19 +7,25 @@ ColorSensor::ColorSensor()
 
 bool ColorSensor::detectBrown()
 {
-  lineSensors.readCalibrated(sensorValues);
+  // Lees de sensorwaarden met readLine() i.p.v. readCalibrated()
+  lineSensors.readLine(sensorValues);
 
+  // Optioneel: print de individuele waarden (voor debuggen)
+  Serial.print("Sensoren: ");
   uint16_t total = 0;
   for (uint8_t i = 0; i < numSensors; i++)
   {
+    Serial.print(sensorValues[i]);
+    Serial.print(" ");
     total += sensorValues[i];
   }
 
+  // Bereken het gemiddelde
   uint16_t average = total / numSensors;
-
-  // 🔍 Debug: print gemiddelde waarde naar de Serial Monitor
+  Serial.print("  Gemiddelde: ");
   Serial.println(average);
 
+  // Vergelijk met drempelwaarden voor bruin
   if (average > brownMin && average < brownMax)
   {
     return true;  // Bruine lijn gedetecteerd
